8a6b1190c4d9b97c6b246608e66e60a5d39d0feb
Ahsan <ahsan.aftab313@gmail.com>
2026-07-06 22:50:29 +0500

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -26,24 +26,26 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
+        for i in range(len(models)):
             model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+            para = param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+            gs = GridSearchCV(
+                estimator=model,
+                param_grid=para,
+                cv=3,
+                n_jobs=-1
+            )
+
+            gs.fit(X_train, y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-
-            #model.fit(X_train, y_train)  # Train model
+            model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
